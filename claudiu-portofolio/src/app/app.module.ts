2b7d1fb65e44f3798346b66867921a6d8df8e2c8
Dascalu Claudiu <C.Dascalu@danieli.com>
2025-01-21 11:23:28 +0200

    feat(portfolio): add portfolio as base class

--- claudiu-portofolio/src/app/app.module.ts
+++ claudiu-portofolio/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
+import { PortofolioComponent } from './portofolio/portofolio.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
